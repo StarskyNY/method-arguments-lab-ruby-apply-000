0597de0544733e0f6744a0d0619d0f236b9af239
Starsky Eustache <star.eu88@gmail.com>
2016-08-14 04:55:25 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+def introduction(name)
+    phrase = "Hi, my name is #{name}."
+    results = puts phrase
+    return results
+end
+
+def introduction_with_language(name,language)
+    phrase = "Hi, my name is #{name} and I am learning to program in #{language}."
+    results = puts phrase
+    return results
+end
